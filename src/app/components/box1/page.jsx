3f58b76c132e217a6b9f283ef7a649bfb96f615c
Adriano <driwcoder@gmail.com>
2023-06-25 13:03:36 -0300

    adicionando projeto sorveteria

--- src/app/components/box1/page.jsx
+++ src/app/components/box1/page.jsx
@@ -2,10 +2,13 @@
 import Image from "next/image";
 import eu from "../../../../public/eu.png";
 import cel from "../../../../public/oculos-cel2.png";
+import cels from "../../../../public/celular-sorveteria2.png";
 import note from "../../../../public/oculos-notebook2.png";
+import notes from "../../../../public/notebook-sorveteria2.png";
 import "./style.css";
-import { BiLogoJavascript, BiLogoReact, BiLogoNodejs, BiServer } from "react-icons/bi";
-import { SiNextdotjs } from "react-icons/si";
+import { BiLogoJavascript, BiLogoReact, BiLogoNodejs, BiServer, BiLogoMongodb } from "react-icons/bi";
+import { SiNextdotjs, SiMysql } from "react-icons/si";
+import { FaSass, FaDatabase, FaGitAlt } from "react-icons/fa";
 
 export default function BoxUm() {
   return (
@@ -20,33 +23,59 @@ export default function BoxUm() {
         </div>
         <div className='skills'>
           <div className='about'>
-            <h3>A little about me</h3>
+            <h3>about me</h3>
             <p>I have one year of experience as a developer, during which I built excellent procljects, using the most advanced technologies in the current market. In addition, I have skills in graphic design, which allows me to offer greater visual quality to my projects.</p>
           </div>
 
-          <ul className='dev-skills'>
+          <div className='boxskills'>
             <h3>Tecnologies</h3>
-            <li className='skill'>
-              <BiLogoReact className='icons' />
-              <p>React.js</p>
-            </li>
-            <li className='skill'>
-              <SiNextdotjs className='icons' />
-              <p>Next.js</p>
-            </li>
-            <li className='skill'>
-              <BiLogoJavascript className='icons' />
-              <p>JavaScript</p>
-            </li>
-            <li className='skill'>
-              <BiLogoNodejs className='icons' />
-              <p>Node.js</p>
-            </li>
-            <li className='skill'>
-              <BiServer className='icons' />
-              <p>Express.js</p>
-            </li>
-          </ul>
+            <ul className='dev-skills'>
+              <div className='dev-skill-le'>
+                <li className='skill'>
+                  <BiLogoReact className='icons' />
+                  <p>React.js</p>
+                </li>
+                <li className='skill'>
+                  <SiNextdotjs className='icons' />
+                  <p>Next.js</p>
+                </li>
+                <li className='skill'>
+                  <BiLogoJavascript className='icons' />
+                  <p>JavaScript</p>
+                </li>
+                <li className='skill'>
+                  <BiLogoNodejs className='icons' />
+                  <p>Node.js</p>
+                </li>
+                <li className='skill'>
+                  <BiServer className='icons' />
+                  <p>Express.js</p>
+                </li>
+              </div>
+              <div className='dev-skills-ld'>
+                <li className='skill'>
+                  <FaSass className='icons' />
+                  <p>Sass</p>
+                </li>
+                <li className='skill'>
+                  <SiMysql className='icons' />
+                  <p>MySQL</p>
+                </li>
+                <li className='skill'>
+                  <BiLogoMongodb className='icons' />
+                  <p>MongoDB</p>
+                </li>
+                <li className='skill'>
+                  <FaDatabase className='icons' />
+                  <p>Database</p>
+                </li>
+                <li className='skill'>
+                  <FaGitAlt className='icons' />
+                  <p>Git</p>
+                </li>
+              </div>
+            </ul>
+          </div>
         </div>
         <div className='projects'>
           <h3>PROJECTS</h3>
@@ -59,6 +88,17 @@ export default function BoxUm() {
             <div className='site-photos'>
               <Image src={cel} alt='celular' />
               <Image src={note} alt='notebook' className='notebook' />
+            </div>
+          </div>
+          <div className='sorveteria'>
+            <h4>IceCream store</h4>
+            <p>Responsive website, built with react components, routed pages with react-router library </p>
+            <a href='https://sorveteria-six.vercel.app/' target='_blank'>
+              project link
+            </a>
+            <div className='site-photos'>
+              <Image src={cels} alt='celular' />
+              <Image src={notes} alt='notebook' className='notebook' />
             </div>
           </div>
         </div>
